08197c7ad78e26295ca5d077eae4ae0e1aa09c54
devmanohar <devmanoharrr@gmail.com>
2025-04-15 20:08:49 +0530

    changes

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 from isend import ISend
-print("✅ Module imported successfully")
+
+client = ISend("API_KEY")
+
+email_response = client.send_email(
+    "WelcomeTemplate",
+    "user@example.com",
+    {"name": "John"}
+)
+print("Email Response:", email_response)
+
+event_response = client.send_event(
+    "Signup",
+    "user@example.com",
+    {"plan": "Free"}
+)
+print("Event Response:", event_response)
